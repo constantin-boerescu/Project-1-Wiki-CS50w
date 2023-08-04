8baa4c5e0a9c40b5e13050b59b74b8936c2c4252
Boerescu Constantin <costi.boerescu14@gmail.com>
2023-08-04 10:30:18 +0300

    Add view entry_page

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -2,9 +2,16 @@ from django.shortcuts import render
 
 from . import util
 
+import markdown2
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
+def entry_page(request, entry):
+    title = markdown2.markdown(util.get_entry(entry))
+    print(title)
+    return render(request, "encyclopedia/entry_page.html",{
+        "entry": title
+    })
