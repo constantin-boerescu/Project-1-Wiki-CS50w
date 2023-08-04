09273e55ecee8e253464d21fef8b9a87867226f2
Boerescu Constantin <costi.boerescu14@gmail.com>
2023-08-04 11:29:53 +0300

    Added the not found message

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -12,10 +12,16 @@ def index(request):
 def entry_page(request, entry):
     '''Renders a page of the encyclopedia entry'''
 
-    
-
     # coverts the .md file format into HTML
-    coverted = markdowner.convert(util.get_entry(entry))
+    try:
+        coverted = markdowner.convert(util.get_entry(entry))
+        
+    # if the entry is None renders a not found message
+    except:
+        return render(request, "encyclopedia/entry_page.html",{
+        "entry_title": "Not found",
+        "not_found": f"{entry.capitalize()} was not found"
+    })
 
     # passing the entry_title and converted HTML code to the entry_page and renders it
     return render(request, "encyclopedia/entry_page.html",{
